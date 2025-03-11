e99a500029da6a824c566746daf5bbe7ef3801fd
neverleaveualong <wmr06244@naver.com>
2025-03-11 16:52:27 +0900

    feat(기능) usercontroller 생성 및 crypto를 이용한 비밀번호 암호화 기능 추가가

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const conn = require("../mariadb");
+const { StatusCodes } = require("http-status-codes");
+const {
+  join,
+  login,
+  passwordResetRequest,
+  passwordReset,
+} = require("../controller/UserController");
 
 router.use(express.json());
 
-router.post("/join", (req, res) => {
-  res.json("회원가입");
-});
-
-router.post("/login", (req, res) => {
-  res.json("로그인");
-});
-
-router.post("/reset", (req, res) => {
-  res.json("비밀번호 초기화 요청");
-});
-
-router.put("/rest", (req, res) => {
-  res.json("비밀번호 초기화");
-});
+router.post("/join", join);
+router.post("/login", login);
+router.post("/reset", passwordResetRequest);
+router.put("/reset", passwordReset);
 
 module.exports = router;
